1984e8b0e2d8c76589a9caad154d5e7a6fbad3cf
Matt Styles <matt@veryfizzyjelly.com>
2015-02-23 13:49:56 +0000

    #add cli

--- index.js
+++
@@ -1,6 +0,0 @@
-
-var app = require( './server/server' );
-var port = process.env.PORT || process.env.npm_package_config_port || 8008;
-
-app.listen( port );
-console.log( 'Listening on port %s', port );
